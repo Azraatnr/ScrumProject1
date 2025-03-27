0e4e1eee4cf89dd5c585e993ca0dad067baa36c3
AnoukvanGemert <anouk.v.gemert@gmail.com>
2025-03-27 15:19:49 +0100

    add create post v1.0

--- src/script.js
+++ src/script.js
@@ -18,8 +18,52 @@ function dropDownMenuPost() {
 
 dropDownMenuPost();
 
+let uploadedImage = '';
+
 function openOwnImage() {
     document.getElementById("fileInput").click();
 }
 
+function showImagePreview(event) {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        uploadedImage = e.target.result
+
+        const img = document.createElement("img");
+        
+        img.src = e.target.result;
+
+        const previewContainer = document.getElementById("imagePreview");
+        previewContainer.innerHTML = "";
+        previewContainer.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+}
+
 document.getElementById('uploadBtn').addEventListener("click", openOwnImage);
+document.getElementById('fileInput').addEventListener("change", showImagePreview);
+
+function showExamplePost() {
+    const getUserMessage = document.getElementById("postNewMessage").value;
+    const pElement = document.getElementById("userMessage");
+    const showImageExample = document.getElementById("showImageExample");
+
+    pElement.innerHTML = "";
+    showImageExample.innerHTML = "";
+
+    pElement.innerHTML = getUserMessage;
+
+    if (uploadedImage) {
+        const imgElement = document.createElement("img");
+
+        imgElement.src = uploadedImage;
+        imgElement.classList.add("w-full", "h-96");
+
+        showImageExample.appendChild(imgElement);
+    }
+
+
+}
+document.getElementById("seeExampleButton").addEventListener("click", showExamplePost);
