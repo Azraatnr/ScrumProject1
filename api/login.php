7baed445207012649b6d10d85bd40f3aeaa243dd
Ties Noordhuis <ties@bit-academy.nl>
2025-04-16 12:22:14 +0200

    login api example

---
+++ api/login.php
@@ -0,0 +1,27 @@
+<?php 
+
+include "connection.php";
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (!empty($email) && !empty($password)) {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $loggedInUser = $stmt->fetch();
+
+        if ($loggedInUser && password_verify($password, $loggedInUser['password'])) {
+            $_SESSION["user_id"] = $loggedInUser["user_id"];
+            echo json_encode(['status' => 'success', 'user_id' => $loggedInUser["user_id"]]);
+            exit();
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid email or password.']);
+            exit();
+        }
+    }
+}
+?>
